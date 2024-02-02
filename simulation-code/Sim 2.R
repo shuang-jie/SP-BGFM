f1a562300b4cfdb39dccd1fdb3ca4ff076b4ef01
Shuangjie Zhang <sz333024@gmail.com>
2024-02-02 00:43:30 -0800

    Add files via upload

--- simulation-code/Sim 2.R
+++ simulation-code/Sim 2.R
@@ -1,5 +1,13 @@
 rm(list = ls())
 
+######################### load real data and obtain empirical zero rate ########
+
+load("Filtered7539OTUs.RData")
+Y1 = Y[, 1:J[1]]; Y2 = Y[, 1:J[2] + J[1]]
+zero.rate1 = apply(Y1, 2, function(x) sum(x==0)/n)
+zero.rate2 = apply(Y2, 2, function(x) sum(x==0)/n)
+rm(list=setdiff(ls(), c("zero.rate1","zero.rate2")))
+
 ########################## load packages #########################
 
 library(statmod)
@@ -32,58 +40,59 @@ pos.phi = function(t.phi, wj){
   ttt = sqrt(zeta[, ki] * t.phi/sum(t.phi) * tau[ki])
   ttt[ttt==0] = sort(ttt[ttt!=0])[1]
   dgamma(t.phi[wj], a.phi, 1, log = T) + sum(dnorm(Lambda[, ki], 0, ttt, log=T)) + log(t.phi[wj])
-} 
+}
+vineBeta <- function(d, b){
+  P = matrix(0, d, d)
+  S = diag(d)
+  ## vine method to generate random covariance
+  for(k in 1:(d-1)){
+    for(i in (k+1):d){
+      P[k, i] = rbeta(1, b, b) 
+      P[k, i] = (P[k, i] - 0.5) * 2
+      if(abs(P[k ,i]) <0.8) {P[k ,i]=0}
+      p = P[k, i]
+      
+      if(k == 1){
+        p = p
+      } else {
+        for(l in (k-1):1 ){
+          p = p * sqrt((1-P[l,i]^2)*(1-P[l,k]^2)) + P[l,i]*P[l,k]
+        }
+      }
+      
+      S[k, i] = p
+      S[i, k] = p
+    }
+  }
+  permutation = sample(1:d, d, replace = F)
+  return(S)
+}
 
 ####################### set up dimension in the paper ############
 
-m = 2
-n = 20
-s = 20
-S = rep(1:s, each = 1)
-J = c(150, 50)
+m = 2; n = 40; s = 20; K.true = 5; seed = 2
+J = c(150, 50); Jsum = sum(J)
+S = rep(1:s, each = 2)
 M = rep(c(1:m), J)
-Jsum = sum(J)
-K.true = 5
-seed = 1
-
-####################### set up factor loading matrix truth #######
-
-set.seed(seed)
-Lambda.true = matrix(0, nrow = Jsum, ncol =K.true)
-sigma2.true = rep(0.25, m)
-
-zero.rate=0.8
-set.seed(seed)
-for(j in 1:25)
-{
-  num=sample(1:5, size = 1, replace = F)
-  ttt = extraDistr::rtnorm(1, 0, 1, -1, 1)
-  ttt = ttt + (ttt>0) * 1 + (ttt<0) * (-1) 
-  Lambda.true[j, num] =  ttt
-}
-
-set.seed(seed+1)
-for(j in 1:25+50)
-{
-  num=sample(1:5, size = 1, replace = F)
-  ttt = extraDistr::rtnorm(1, 0, 1, -1, 1)
-  ttt = ttt + (ttt>0) * 1 + (ttt<0) * (-1) 
-  Lambda.true[j, num] =  ttt
-}
-
-set.seed(seed+2)
-for(j in 1:25+150)
-{
-  num=sample(1:5, size = 1, replace = F)
-  ttt = extraDistr::rtnorm(1, 0, 1, -1, 1)
-  ttt = ttt + (ttt>0) * 1 + (ttt<0) * (-1) 
-  Lambda.true[j, num] =  ttt
-}
 
 ####################### set up covariance matrix truth ###########
 
-Omega.true = Lambda.true %*% t(Lambda.true) + diag(rep(sigma2.true, J))
-true.cor <- cov2cor(Omega.true)
+set.seed(seed)
+sigma2.true = rep(1, m)
+
+set.seed(seed)
+ttt = vineBeta(Jsum, 1)
+ttt = as.matrix(Matrix::nearPD(ttt)$mat)
+true.cor = ttt
+
+sigma2.true = runif(Jsum, 1, 1.5)
+Omega.true <- diag(rep(1, Jsum))
+for(i in 1:Jsum){
+  for(j in 1:Jsum){
+    Omega.true[i,j] <- 
+      true.cor[i, j] * sqrt( sigma2.true[i] * sigma2.true[j] )
+  }
+}
 
 ####################### set up ri alpha_Sij truth ################
 
@@ -92,8 +101,18 @@ ri.true = matrix(runif(m*n, 0, 2), m, n)
 location.mean.alpha.true = matrix(0, Jsum, 3)
 set.seed(seed)
 Weights = matrix(0, Jsum, 3)
-for(j in 1:Jsum){
-  Weights[j,] = extraDistr::rdirichlet(1, alpha=c(30, 40, 30))
+id1 = sample(1:length(zero.rate1), J[1], replace = T)
+for(j in 1:J[1]){
+  ww = zero.rate1[id1[j]]
+  if(ww==0){ww = 0.0000001}
+  Weights[j,] = extraDistr::rdirichlet(1, alpha=c(ww*100, (1-ww)*0.6*100,  (1-ww)*0.4*100))
+}
+set.seed(seed)
+id2 = sample(1:length(zero.rate2), J[2], replace = T)
+for(j in 1:J[2]){
+  ww = zero.rate2[id2[j]]
+  if(ww==0){ww = 0.0000001}
+  Weights[j+J[1],] = extraDistr::rdirichlet(1, alpha=c(ww*100, (1-ww)*0.6*100,  (1-ww)*0.4*100))
 }
 location.mean.alpha.true[,1] = -5
 set.seed(seed)
@@ -110,7 +129,29 @@ for(ii in 1:n){
 }
 alphaij.true = alphaSij.true[S, ]
 
-########################## set up mean truth ########################
+####################### set up beta truth ########################
+
+set.seed(seed)
+X = cbind( rep(c(1,0), n/2), rep(c(0,1), n/2))
+p = ncol(X)
+beta_jp_truth <- matrix(0, nrow = Jsum, ncol = p)
+for(j in 1:Jsum){
+  delta = LaplacesDemon::rbern(1, 0.8)
+  if(delta == 1){
+    beta_jp_truth[j, 1] = 0 
+  } else {
+    c_v= rnorm(1, 0, sqrt(1/3)) 
+    if(c_v >0){
+      c_v = c_v + 1
+    } else {
+      c_v = c_v - 1
+    }
+    beta_jp_truth[j, 1] = c_v
+  }
+}
+beta_jp_truth[,2] = 0
+
+########################## set up mu(mean) truth ########################
 
 mu.true = matrix(0, n, Jsum)
 for(mi in 1:m){
@@ -121,6 +162,7 @@ for(mi in 1:m){
     }
   }
 }
+mu.true = mu.true + tcrossprod(X, beta_jp_truth)
 
 ########################## simulate count data #######################
 
@@ -133,10 +175,10 @@ Y = floor(exp(y.star.true))
 
 ######################### prior hyper-parameter #########################
 
-K = 10; a.sig = 3; b.sig = 3; a.phi = 1/20 
+K = 15; a.sig = 3; b.sig = 3; a.phi = 1/20 
 a.tau = 0.1; b.tau = 1/Jsum; acc.tar = 0.234
 
-######################### prior mean hyper-parameter #########################
+######################### prior mean hyper-parameter ####################
 
 hat.ri = matrix(NA, nrow = m, ncol = n)
 for(mi in 1:m){
@@ -157,7 +199,7 @@ for(i in 1:s){
 
 ### ri hyper-parameter ###
 nu.r = rowMeans(hat.ri)
-Lr = 30; a.psi.r = 1; a.w = b.w = 1; ur2 = 1; a.xi = nu.r
+Lr = 30; a.psi.r = 1; a.w = b.w = 5; ur2 = 1; a.xi = nu.r
 ri = hat.ri
 Si1 = matrix(NA, m, n)
 for(mi in 1:m){
@@ -178,7 +220,7 @@ sig2.xi.r = rep(1, m)
 
 ### alphai hyper-parameter ###
 nu.alpha = hat.alpha
-L.alpha = 35; a.psi.alpha = 1; a.w.alpha = b.w.alpha = 1; 
+L.alpha = 35; a.psi.alpha = 1; a.w.alpha = b.w.alpha = 5; 
 u2.alpha = Rfast::colVars(hat.alphasij)
 a.xi.alpha = nu.alpha
 set.seed(seed)
@@ -241,7 +283,11 @@ J_bound = J
 J_bound[1] = J_bound[1] -1
 J_bound = c(0, cumsum(J_bound))
 
-RSS = y.star - ri.muij - alphaij - tcrossprod(eta, Lambda)
+mu_beta = matrix(0, nrow = p, ncol=1)
+u2_beta = 3
+beta_jp <- matrix(0, nrow = Jsum, ncol = p)
+
+RSS = y.star - ri.muij - alphaij - tcrossprod(eta, Lambda)  - tcrossprod(X, beta_jp)
 
 ######################### create storage ###################
 
@@ -262,11 +308,11 @@ sig2.st <- matrix(NA, m, nsamp)
 Sij1.st <- array(NA, dim=c(s, Jsum, nsamp))
 Sij2.st <- array(NA, dim=c(s, Jsum, nsamp))
 eta.st <- array(NA, dim=c(n, K, nsamp))
+beta.st <- array(NA, dim=c(Jsum, p, nsamp))
 
 ######################### run MCMC #########################
 
 for(ni in 1:niter){
-  
   # impute continuous latent variable
   RSS = y.star - RSS
   y.star = update_ystar(log.Y, log.Y1, n, Jsum, RSS, sig2, M-1)
@@ -317,8 +363,7 @@ for(ni in 1:niter){
   ri.muij = t(ri)[,rep(1:m, J)]
   RSS = RSS - ri.muij
   
-  
-  ## update alphai
+  ## update alphai related
   try = update_psi_w(m, L.alpha, J_bound = J_bound, Sij1-1, Sij2, a.w.alpha, 
                      b.w.alpha, a.psi.alpha)
   psi.alpha = adrop(try[,,1,drop = F], drop = 3)
@@ -341,6 +386,15 @@ for(ni in 1:niter){
   alphaij = alphasij[S, ]
   RSS = RSS - alphaij
   
+  ## update beta
+  RSS = RSS + tcrossprod(X, beta_jp)
+  for(ji in 1:Jsum){
+    U.l = chol(diag(rep(1/u2_beta, p), nrow = p, ncol = p) + crossprod(X) /sig2[M[ji]] )
+    a.l = crossprod(X, RSS[, ji]) / sig2[M[ji]]
+    beta_jp[ji, ] = backsolve(U.l, backsolve(U.l, a.l, transpose = T) + rnorm(p))
+  }
+  RSS = RSS - tcrossprod(X, beta_jp)
+  
   ## save result
   if((ni > niter/2) & (ni%%5 ==0)){
     count.st = count.st + 1
@@ -352,6 +406,7 @@ for(ni in 1:niter){
     Sij1.st[,, count.st] <- Sij1
     Sij2.st[,, count.st] <- Sij2
     sig2.st[, count.st] <- sig2
+    beta.st[,, count.st] <- beta_jp
     
     Lambda.st[,, count.st] <- Lambda
     tau.st[,count.st] <- tau
@@ -360,7 +415,6 @@ for(ni in 1:niter){
   }
 }
 
-save.image("Sim 1.RData")
-
+save.image("Sim 2.RData")
 
 
